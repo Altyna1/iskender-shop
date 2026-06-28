e03ec8c217db120087efbb30dc9390ee2034f751
Altyna1 <zoldosbekovaaltynaj30@gmail.com>
2026-06-28 14:46:50 +0600

    feat: add application models

---
+++ src/app/core/models/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  oldPrice?: number;
+  currency: string;
+  inStock: boolean;
+  badge?: string;
+  isFavorite?: boolean;
+  quantity?: number;
+}
---
+++ src/app/core/models/store.model.ts
@@ -0,0 +1,6 @@
+export interface Store {
+  id: number;
+  city: string;
+  address: string;
+  phone: string;
+}
