b7f8090646563c1852dad5a955a7bdfdc0662e9d
Altyna1 <zoldosbekovaaltynaj30@gmail.com>
2026-06-28 15:02:52 +0600

    Реализован компонент карточки товара

--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../core/models/product.model';
+import { Counter } from '../counter/counter';
+import { Badge } from '../badge/badge';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [Counter, Badge],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
-export class ProductCard {}
+export class ProductCard {
+  @Input({ required: true }) product!: Product;
+
+  @Input() variant: 'default' | 'auction' = 'default';
+
+  @Output() addToCart = new EventEmitter<Product>();
+
+  addProduct(): void {
+    this.addToCart.emit(this.product);
+  }
+}
