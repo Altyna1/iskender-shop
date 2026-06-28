5cc4eaef312a66f1453778043437c7ab3f9f615d
Altyna1 <zoldosbekovaaltynaj30@gmail.com>
2026-06-28 15:00:57 +0600

    Реализован компонент бейджа

--- src/app/shared/components/badge/badge.ts
+++ src/app/shared/components/badge/badge.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-badge',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './badge.html',
   styleUrl: './badge.scss',
 })
-export class Badge {}
+export class Badge {
+  @Input() text = '';
+}
